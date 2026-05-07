9bbc020e8f89b84c922ca81a354114fe35dd11d4
k2viewer <nakae198@gmail.com>
2026-05-07 15:21:39 +0200

    Update next-env.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,8 @@
-/// <reference types="next" />
-/// <reference types="next/navigation" />
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Dies stellt sicher, dass Next.js nicht versucht, 
+  // die Seite statisch zu exportieren, was oft zu 404s führt
+  output: 'standalone',
+};
+
+export default nextConfig;
